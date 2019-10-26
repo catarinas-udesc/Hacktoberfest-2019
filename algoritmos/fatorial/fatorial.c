88ae9638c7d039d6f73d66ae73fb15d6bb8c53e0
Nicolas Peter Lane <n3ck@tutanota.com>
2019-10-25 21:05:56 -0300

    Fatorial por exaustao usando laco de repeticao

---
+++ algoritmos/fatorial/fatorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main (void){
+    int valor;// declaração de variavel
+    scanf("%d", &valor);
+    int i;
+    int rest = 1; // ponto de acumulo do valor
+    for (i=1;i<=valor;i++){
+        rest *= valor;//repetição por exaustao
+    }
+    return 0;
+}
